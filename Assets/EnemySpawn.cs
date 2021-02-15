50003935339aca1c59e9d95ac06bec980700b597
Liran Shoshana <liranshoshana93@gmail.com>
2021-02-15 09:32:13 +0200

    first commit

---
+++ Assets/EnemySpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject enemy;
+    public bool[] alreadyUsed;
+    public int randomIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        alreadyUsed = new bool[spawnPoints.Length];
+        for (int i = 0; i < 4; i++)
+        {
+            randomIndex = Random.Range(0, alreadyUsed.Length);
+            Debug.Log(randomIndex);
+            while (alreadyUsed[randomIndex] == true)
+            {
+                randomIndex = Random.Range(0, alreadyUsed.Length);
+            }
+            alreadyUsed[randomIndex] = true;
+            Instantiate(enemy, spawnPoints[randomIndex].position, Quaternion.identity);
+        }
+    }
+
+}
